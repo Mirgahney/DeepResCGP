1c04edb98562fcf57e0de456d7c321b003abd82a
Mirgahney <mmerghaney@aimsammi.org>
2019-03-31 05:22:13 +0200

    fix res call

--- conv_gp/models.py
+++ conv_gp/models.py
@@ -312,7 +312,7 @@ class ModelBuilder(object):
             layers.append(conv_layer)
 
             for i in range(res_blocks): #H_X, M, feature_map, filter_size , stride , layer_params, name = 'unit'
-                conv_layer, H_X = self._residual_block(H_X, M, feature_map, 3, 1, layer_params,  ('unit ' + str(i+1)))
+                conv_layer, H_X = self._residual_block(H_X = H_X, M = M, feature_map = feature_map, filter_size = 3, stride = 1, layer_params = layer_params,  name = ('unit ' + str(i+1)))
                 layers.append(conv_layer)
 
         return layers, H_X
