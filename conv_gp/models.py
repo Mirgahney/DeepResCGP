b713094c7e15ecb109f2403a839945bdfac66c82
Mirgahney <mmerghaney@aimsammi.org>
2019-04-10 09:47:43 +0200

    debugg

--- conv_gp/models.py
+++ conv_gp/models.py
@@ -23,7 +23,7 @@ def image_HW(patch_count):
     print(type(patch_count))
     if type(patch_count) == tf.Dimension :
         print(patch_count.as_list())
-        print(type(patch_count.as_list()[0])
+        print(type(patch_count.as_list()[0]))
         print(type(tf.cast(patch_count, tf.int32)) == tf.Tensor)
 
     print(patch_count)
