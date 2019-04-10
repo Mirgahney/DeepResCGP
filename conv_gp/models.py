fbc4afc4af3b60b0fd238ec08498a918f12dd25e
Mirgahney <mmerghaney@aimsammi.org>
2019-04-10 10:24:40 +0200

    debugg

--- conv_gp/models.py
+++ conv_gp/models.py
@@ -37,7 +37,8 @@ def identity_conv(NHWC_X, filter_size, feature_maps_in, feature_maps_out, stride
         batch = tf.dimension_value(NHWC_X.shape[0])
         print(type(batch))
         print(batch)
-        print(type(NHWC_X.eval()))
+        with sess.as_default():
+            print(type(NHWC_X.eval()))
     else:
         batch = NHWC_X.shape[0]
         print(type(batch))
