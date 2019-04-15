2c44ec55ca5b882c69ff4b752364243f56708b77
Mirgahney <mmerghaney@aimsammi.org>
2019-04-15 13:10:57 +0200

    debugg

--- conv_gp/models.py
+++ conv_gp/models.py
@@ -418,7 +418,7 @@ class ModelBuilder(object):
                 # H_X_pad = np.pad(H_X, pad_width=npad, mode='constant', constant_values=0)
                 # pad_layer = lambda x: np.pad(x, pad_width=npad, mode='constant', constant_values=0)
                 # layers.append(pad_layer)
-                conv_layer, H_X = self._conv_layer(H_X, M, feature_map, 3, 1, 'SAME', layer_params)
+                conv_layer, H_X = self._conv_layer(H_X, M, feature_map, 3, 1, 'VALID', layer_params)
                 shapes.append(H_X.shape)
                 # print(conv_layer)
                 layers.append(conv_layer)
