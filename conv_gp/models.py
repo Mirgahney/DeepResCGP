628db148e99cd029e5be5f487b16af3215782868
Mirgahney <mmerghaney@aimsammi.org>
2019-04-15 13:17:10 +0200

    debugg

--- conv_gp/models.py
+++ conv_gp/models.py
@@ -406,15 +406,10 @@ class ModelBuilder(object):
                 # H_X_pad = np.pad(H_X, pad_width=npad, mode='constant', constant_values=0)
                 # pad_layer = lambda x: np.pad(x, pad_width=npad, mode='constant', constant_values=0)
                 # layers.append(pad_layer)
-                shortcut = H_X
-                conv_layer, H_X = self._conv_layer(shortcut, M, feature_map, 3, 1, 'VALID', layer_params)
-                layers.append(conv_layer)
-                
-                conv_layer, H_X = self._conv_layer(shortcut, M, feature_map, 3, 1, 'VALID', layer_params)
-                print('sam padding ', H_X.shape)
+                conv_layer, H_X = self._conv_layer(H_X, M, feature_map, 3, 1, 'SAME', layer_params)
                 shapes.append(H_X.shape)
                 # print(conv_layer)
-                
+                layers.append(conv_layer)
                 # H_X_pad = np.pad(H_X, pad_width=npad, mode='constant', constant_values=0)
                 # pad_layer = lambda x: np.pad(x, pad_width=npad, mode='constant', constant_values=0)
                 # layers.append(pad_layer)
