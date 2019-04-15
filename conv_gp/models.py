b7aee5d69e617b50f5b2f305bfdec679d164c91c
Mirgahney <mmerghaney@aimsammi.org>
2019-04-15 11:32:37 +0200

    debugg

--- conv_gp/models.py
+++ conv_gp/models.py
@@ -318,7 +318,7 @@ class ModelBuilder(object):
             filter_size = filter_sizes[i]
             stride = strides[i]
             layer_params = loaded_parameters.get(i)
-            conv_layer, H_X = self._conv_layer(H_X, M, feature_map, filter_size, stride, layer_params)
+            conv_layer, H_X = self._conv_layer(H_X, M, feature_map, filter_size, stride, 'VALID', layer_params)
             layers.append(conv_layer)
             shapes.append(H_X.shape)
         print(shapes)
@@ -336,7 +336,7 @@ class ModelBuilder(object):
             stride = strides[i]
             layer_params = loaded_parameters.get(i)
             
-            conv_layer, H_X = self._conv_layer(H_X, M, feature_map, filter_size, stride, layer_params)
+            conv_layer, H_X = self._conv_layer(H_X, M, feature_map, filter_size, stride, 'VALID', layer_params)
             shapes.append(H_X.shape)
             # print(conv_layer)
             layers.append(conv_layer)
